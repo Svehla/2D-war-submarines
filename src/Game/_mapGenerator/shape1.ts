7b2b04acd692c2f0633f63396f954ce75dacd889
Jakub Švehla <svehl.jakub@gmail.com>
2020-05-06 14:55:04 +0200

    add map generator

---
+++ src/Game/_mapGenerator/shape1.ts
@@ -0,0 +1,64 @@
+// import { getLinesFromShape } from './mapGenerator/_mapGenerator'
+// import shape1 from './mapGenerator/_shape1'
+
+// const myPolygon = getLinesFromShape(shape1, {
+//   x: 28,
+//   y: 0,
+// })
+
+export default `
+                          XXXXXXXXXX                                                |
+                         XX        X                                                |
+                         X          XXX                                             |
+                       XX              X                                            |
+      XXXXXXXXXX      X                 X              XXXXXX                       |
+     X         XXX   XX                  X           XXX     X                      |
+ XXXX            XXXXX                    XXXXXXXXXXXX        X                     |
+X                                                             X                     |
+X                                                              X                    |
+X                                                              X                    |
+ XXXXXXX                                                     XX                     |
+        XXXXXXX                                            XX                       |
+               XXXXXX                                     X                         |
+                    X                                  XXX                          |
+                    X                                 X                             |
+                 XXX                              XXXX                              |
+                X                             XXXX                                  |
+                X                            X                                      |
+                X           XXXXX            X                                      |
+                 XXX       X     XXXX         XX                                    |
+                   XXXXXXXX          X          XX                                  |
+                                    X            X                                  |
+                                   X              X                                 |
+                                   X              X                                 |
+                                   X             X                                  |
+                                   X             X                                  |
+                                   X             X                                  |
+                                   X          XXX                                   |
+                                    XXXXXXXXXX                                      |
+                                                                                    |
+                                                                                    |
+                                                                                    +
+
+`
+
+// export default `
+//                                                 |
+// +---+                                           |
+// |   |                                           |
+// |   |                                           |
+// |   |                                           |
+// |   |                                           |
+// |   +----------+                                |
+// |              |                                |
+// |              |                                |
+// |              +------+                         |
+// |                     |                         |
+// |                     +-------------+           |
+// |                                   |           |
+// +-------+                           |           |
+//         |              +------------+           |
+//         |              |                        |
+//         +--------------+                        |
+//                                                 |
+//                                                 `
